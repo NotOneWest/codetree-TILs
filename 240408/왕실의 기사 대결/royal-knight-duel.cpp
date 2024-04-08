208994b6c0ee122bf5330ac604cbaf0271372c4c
HanSeo An <gkstj0604@naver.com>
2024-04-08 21:53:55 +0900

    Title: 왕실의 기사 대결, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240408/왕실의 기사 대결/royal-knight-duel.cpp
+++ 240408/왕실의 기사 대결/royal-knight-duel.cpp
@@ -1,76 +1,70 @@
 #include <iostream>
 #include <cstring>
+#include <queue>
 using namespace std;
 
 struct Knight{
     int r, c, h, w, k, d;
 };
 Knight knights[31];
-int canMove[31];
+int damage[31];
 int board[41][41];
-int k_board[41][41];
+int canMove[31]; // 이동 가능한 idx
+int nr[31], nc[31]; // 다음 위치 저장
 int l, n, q;
 
-int dx[4] = {-1, 0, 1, 0};
-int dy[4] = {0, 1, 0, -1};
+int dr[4] = {-1, 0, 1, 0};
+int dc[4] = {0, 1, 0, -1};
 
 bool isRange(int x, int y){ return (x < 0 || x >= l || y < 0 || y >= l); }
 
-bool isMove(int idx, int d){
-    int x, y;
-    if(d == 0){ x = knights[idx].r; y = knights[idx].c; }
-    else if(d == 1){ x = knights[idx].r; y = knights[idx].c + knights[idx].w - 1; }
-    else if(d == 2){ x = knights[idx].r + knights[idx].h - 1; y = knights[idx].c; }
-    else{ x = knights[idx].r; y = knights[idx].c; }
+bool isMove(int idx, int dir){
+    for(int i=1; i<=n; i++){
+        damage[i] = 0;
+        canMove[i] = 0;
+        nr[i] = knights[i].r; nc[i] = knights[i].c;
+    }
 
-    if(d % 2){
-        for(int i=0; i<knights[idx].h; i++){
-            int nx = x + i + dx[d], ny = y + dy[d];
-            if(isRange(nx, ny)) return false;
-            if(board[nx][ny] == 2) return false;
-            if(k_board[nx][ny] >= 10 && !canMove[k_board[nx][ny] / 10]){
-                if(!isMove(k_board[nx][ny] / 10, d)) return false;
-                else canMove[k_board[nx][ny] / 10] = 1;
+    queue<int> q;
+    q.push(idx);
+    canMove[idx] = 1;
+
+    while(!q.empty()){
+        int curr = q.front(); q.pop();
+
+        nr[curr] += dr[dir]; nc[curr] += dc[dir];
+
+        if(isRange(nr[curr], nc[curr])) return false;
+        for(int i = nr[curr]; i <= nr[curr] + knights[curr].h - 1; i++) {
+            for(int j = nc[curr]; j <= nc[curr] + knights[curr].w - 1; j++) {
+                if(board[i][j] == 1) damage[curr]++;
+                if(board[i][j] == 2) return false;
             }
         }
-    } else{
-        for(int i=0; i<knights[idx].w; i++){
-            int nx = x + dx[d], ny = y + i + dy[d];
-            if(isRange(nx, ny)) return false;
-            if(board[nx][ny] == 2) return false;
-            if(k_board[nx][ny] >= 10 && !canMove[k_board[nx][ny] / 10]){
-                if(!isMove(k_board[nx][ny] / 10, d)) return false;
-                else canMove[k_board[nx][ny] / 10] = 1;
-            }
+
+        for(int i=1; i<=n; i++){
+            if(canMove[i] || knights[i].k <= knights[i].d) continue;
+            if(knights[i].r > nr[curr] + knights[curr].h - 1 || nr[curr] > knights[i].r + knights[curr].h - 1) continue;
+            if(knights[i].c > nc[curr] + knights[curr].w - 1 || nc[curr] > knights[i].c + knights[curr].w - 1) continue;
+            
+            canMove[i] = true;
+            q.push(i);
         }
     }
 
+    damage[idx] = 0;
     return true;
 }
 
-void moving(int idx, int d){
-    memset(canMove, 0, sizeof(canMove));
-    if(!isMove(idx, d)) return;
+void moving(int idx, int dir){
+    if(knights[idx].k <= knights[idx].d) return;
+    if(!isMove(idx, dir)) return;
 
-    canMove[idx] = 1;
     for(int i=1; i<=n; i++){
-        if(canMove[i]){ knights[i].r += dx[d]; knights[i].c += dy[d]; }
-    }
-}
-
-void update(int start){
-    memset(k_board, 0, sizeof(k_board));
-    for(int i=1; i<=n; i++){
-        for(int x=0; x<knights[i].h; x++){
-            for(int y=0; y<knights[i].w; y++){
-                k_board[knights[i].r+x][knights[i].c+y] = i * 10;
-                if(canMove[i]){
-                    if(i != start){
-                        if(board[knights[i].r+x][knights[i].c+y] == 1) knights[i].d++;
-                    }
-                }
-                
-            }
+        if(canMove[i]){
+            knights[i].r = nr[i];
+            knights[i].c = nc[i];
+            knights[i].d += damage[i];
         }
     }
 }
@@ -84,24 +78,18 @@ int main() {
     for(int i=1; i<=n; i++){
         int r, c, h, w, k; cin >> r >> c >> h >> w >> k;
         knights[i] = {r-1, c-1, h, w, k, 0};
-        for(int x=0; x<h; x++){
-            for(int y=0; y<w; y++){
-                k_board[r-1+x][c-1+y] = i * 10;
-            }
-        }
     }
 
     for(int i=0; i<q; i++){
         int a, b; cin >> a >> b;
-        if(knights[a].k <= knights[a].d) continue;
-
         moving(a, b);
-        update(a);
     }
 
-    int ans = 0;
-    for(int i=1; i<=n; i++){
-        if(knights[i].k > knights[i].d) ans += knights[i].d;
+    long long ans = 0;
+    for(int i = 1; i <= n; i++) {
+        if(knights[i].k > knights[i].d) {
+            ans += knights[i].d;
+        }
     }
     cout << ans;
     return 0;
